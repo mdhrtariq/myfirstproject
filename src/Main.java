63537aac1a022bb0436342cb8fd807dbf0d88f81
mdhrtariq <thariq22@gmail.com>
2022-08-11 19:28:18 +0530

    addition prefix postfix

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+package com.Forloopconcept;
+public class Main {
+    public static void main(String[] args)
+    {
+        int a=10, b=20, c=25;
+        {
+        System.out.println(b-- + c++ + ++a + --b + a++ + ++a);
+                         //(20+25+11+18+11+13)=98)
+    }
+    }
+}
