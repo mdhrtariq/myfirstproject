12ae8afdf114b3abbf454a0719c8a6ebf661c16f
mdhrtariq <thariq22@gmail.com>
2022-08-12 14:04:42 +0530

    for loop concept

--- src/com/Forloopconcept/Main.java
+++ src/com/Forloopconcept/Main.java
@@ -5,7 +5,9 @@ public class Main {
         int a=10, b=20, c=25;
         {
         System.out.println(b-- + c++ + ++a + --b + a++ + ++a);
-                         //(20+25+11+18+11+13)=98)
+            System.out.println(++b + a-- - c++ + ++a + ++b);
+                         //(20+25+11+18+11+13)=98
+                        // (19+13-26+13+20)=39
     }
     }
 }
