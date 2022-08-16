d3adc696ad206dc8e5c86dd0787d8fe6a39daf8f
mdhrtariq <110968471+mdhrtariq@users.noreply.github.com>
2022-08-16 18:48:32 +0530

    maths

--- Main.java
+++ Main.java
@@ -1,13 +1,52 @@
-package com.Forloopconcept;
+
+package com.maths;
 public class Main {
-    public static void main(String[] args)
-    {
-        int a=10, b=20, c=25;
-        {
-        System.out.println(b-- + c++ + ++a + --b + a++ + ++a);
-            System.out.println(++b + a-- - c++ + ++a + ++b);
-                         //(20+25+11+18+11+13)=98
-                        // (19+13-26+13+20)=39
-    }
+    public static void main(String[] args) {
+        int m=4;
+        int n=7;
+         int t1=m+n;
+         int t2=m-n;
+         int t3=m*n;
+         int t4=m/n;
+         int t5=m%n;{
+            System.out.println(t1);
+            System.out.println(t2);
+            System.out.println(t3);
+            System.out.println(t4);
+            System.out.println(t5);
+        }
     }
 }
+//        int id
+//    }
+////        int age =19;
+//
+//        if (age >= 18)
+//        {
+//            System.out.println("eligible for vote");
+//        }
+//        else if (age<=18)
+//        {
+//            System.out.println("not eligible for vote");
+//        }
+//    }
+//}
+
+//        for (int i = 0; i <= 10; i++) {
+//            for (int j = 1; j <= 1; j++) {
+//                System.out.println(i + "    " + j);
+//
+//
+//                }
+//
+//        }
+//        {
+//            int a = 12;
+//            int b = 15;
+//            System.out.println(a + b);
+//            System.out.println(12 + 15);
+//            System.out.println(45 - 35 / 2 * 8);
+//
+
+
+
